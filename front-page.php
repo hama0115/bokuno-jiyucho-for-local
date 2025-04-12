6f6dbad6a9a9cb62fd203693417eb17209f4792d
hama0115 <oh.akibee1231@gmail.com>
2025-04-12 20:52:47 +0900

    プロフィール欄をテンプレートパーツにした

--- front-page.php
+++ front-page.php
@@ -56,21 +56,9 @@
           <div class="btn-area"><button class="page-btn"><a href="<?php echo esc_url(home_url('/latest-posts'))?>">もっとみる</a></button></div>
         </div>
       </div>      
-      <div class="content-wrapper">
-        <div class="content">
-          <section class="profile-area">
-            <h2 class="profile-top">プロフィール</h2>
-            <div class="profile-img-area">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/sample-thumbnail.jpg" alt="プロフィール画像">
-              <div class="profile-name-wrapper">
-                <p class="profile-name">アル</p>
-              </div>
-            </div>
-            <p class="profile-description">1985年あたりに生まれ、武庫川大学歴史学科を卒業。いくらでも寝ることができます。</p>
-          </section>
-          
-        </div>
-      </div>
+      
+      //プロフィール欄を表示
+      <?php get_template_part('template-parts/profile-section'); ?>
     </main>
 
 <?php get_footer(); ?>
---
+++ template-parts/profile-section.php
@@ -0,0 +1,14 @@
+<div class="content-wrapper">
+  <div class="content">
+    <section class="profile-area">
+      <h2 class="profile-top">プロフィール</h2>
+      <div class="profile-img-area">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/sample-thumbnail.jpg" alt="プロフィール画像">
+        <div class="profile-name-wrapper">
+          <p class="profile-name">アル</p>
+        </div>
+      </div>
+      <p class="profile-description">1985年あたりに生まれ、武庫川大学歴史学科を卒業。いくらでも寝ることができます。</p>
+    </section>          
+  </div>
+</div>
